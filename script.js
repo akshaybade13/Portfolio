242e9e0a07a536ce00beadb8e3e3f934c5833c1d
akshaybade13 <128242158+akshaybade13@users.noreply.github.com>
2023-12-29 23:03:52 +0530

    Add files via upload

---
+++ script.js
@@ -0,0 +1,42 @@
+// MOBILE SCREEN SIDE MENU
+var sideMenu =document.getElementById("sidemenu");
+
+function openmenu(){
+    sideMenu.style.right='0';
+}
+
+function closemenu(){
+    sideMenu.style.right='-200px';
+}
+
+
+// FOR ANIMATION
+const observer=new IntersectionObserver((entries)=>{
+    entries.forEach((entry)=>{
+        console.log(entry)
+        if(entry.isIntersecting){
+            entry.target.classList.add('show');
+        }else{
+            entry.target.classList.remove('show');
+        }
+    });
+});
+
+const hiddenElements =document.querySelectorAll('.hidden');
+hiddenElements.forEach((el)=>observer.observe(el));
+
+// ABOUT LINK
+
+var aboutLink =document.getElementsByClassName('alinks');
+var linkContent =document.getElementsByClassName('link-content');
+
+function openlink(linkID){
+    for(alink of aboutLink){
+        alink.classList.remove('active-link');
+    }
+    for(linkc of linkContent){
+        linkc.classList.remove('active');
+    }
+    event.currentTarget.classList.add('active-link');
+    document.getElementById(linkID).classList.add('active');
+}
